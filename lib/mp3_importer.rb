9692aad5cc8daa79441275242a2c093866b3db42
Bryce B <bryce.boddy@gmail.com>
2020-05-02 22:44:14 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -7,11 +7,19 @@ class MP3Importer
   end
   
   def files
+<<<<<<< HEAD
     Dir.entries("#{path}").select { |f| f.include? ".mp3"}
   end
   
   def import
     files.each {|song| Song.new_by_filename(song)}
+=======
+    Dir["./spec/fixtures/mp3s/**/*.mp3"]
+  end
+  
+  def import
+    
+>>>>>>> 68d6120899b53ef1c15fcad726bc6247d760b370
   end 
   
 end
